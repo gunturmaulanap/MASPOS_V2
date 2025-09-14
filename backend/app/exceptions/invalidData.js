2f4c39e56fe65a135ec6c90d2692cac91148b387
gunturmaulanap <95459541+gunturmaulanap@users.noreply.github.com>
2025-09-14 17:25:46 +0700

    Delete backend directory

--- backend/app/exceptions/invalidData.js
+++
@@ -1,7 +0,0 @@
-module.exports = class InvalidData extends Error {
-  constructor(message, code = 422) {
-    super(message);
-    this.message = message || "Terjadi kesalahan pada data yang dikirimkan";
-    this.code = code;
-  }
-};
--- backend/app/models/Knex.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const env = process.env.NODE_ENV || "development";
-const config = require("../../knexfile")[env];
-
-module.exports = require("knex")(config);
